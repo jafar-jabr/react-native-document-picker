5b335f2c25c6006953b105242f5391b958757b11
Jafar jabr <jafar.jabr@punct.ro>
2021-04-04 00:50:15 +0300

    build: update ios

--- ios/RNDocumentPicker.m
+++ ios/RNDocumentPicker.m
@@ -4,11 +4,21 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(doPicking: callback: (RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(doPicking:cb: (RCTResponseSenderBlock)callback)
 {
   @try {
      NSNumber *errorCode = [NSNumber numberWithInt:301];
-     callback(@[[NSNull null], errorCode]);
+     NSString *errorMessage = @"here is objc 2";
+     NSDictionary *errorDict = @{ @"code" : errorCode,
+                              @"message" : errorMessage
+      };
+      NSDictionary *responseDict = @{ @"path" : @"-",
+                              @"fileName" : @"-",
+                              @"fileSize" : [NSNumber numberWithInt:666],
+                              @"type" : @"-"
+      };
+    // callback(@[errorDict, [NSNull null]]);
+     callback(@[errorDict, responseDict]);
   }
   @catch ( NSException *e ) {
       NSNumber *errorCode = [NSNumber numberWithInt:301];
